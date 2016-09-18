457b9b5e0261e01500f3d8fc2cb5ea81c8c79eee
Sebastien Rancoud <s.rancoud@lonsdale.fr>
2016-09-18 21:47:29 +0200

    update bot

---
+++ ats.js
@@ -0,0 +1,5 @@
+// load server
+// redirect user vers authorize url
+// retreive "code"
+// authenticate with "code" on another url
+// sometimes I have to load access token with no user (twitter case)
