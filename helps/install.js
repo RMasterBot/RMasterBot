232250a37cdc6350841b2d63080b3a589dd8e378
Sebastien Rancoud <s.rancoud@lonsdale.fr>
2016-11-11 01:04:28 +0100

    moving help files in tasks and job done

--- helps/install.js
+++
@@ -1,9 +0,0 @@
-console.log("\n" + 'Install bot for RMasterBot.');
-
-console.log("\n" + 'Usage:');
-console.log("    " + 'node install <bot_name>    install a bot from RMasterBot list');
-console.log("    " + 'node install <url>         install a custom bot with an url to a zip file');
-
-console.log("\n" + 'Arguments:');
-console.log("    " + 'bot_name    bot name from RMasterBot list: check in README');
-console.log("    " + 'url         url to a zip file built for RMasterBot OR just github_account_name/repository_name');
